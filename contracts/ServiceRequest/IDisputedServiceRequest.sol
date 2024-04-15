8da5e75dfb0a5705b419ebd94780de252a44024c
ArpitAkay <arpitkumar4000@gmail.com>
2024-04-15 16:03:35 +0530

    Added validation for decideWinnerForDispute

--- contracts/ServiceRequest/IDisputedServiceRequest.sol
+++ contracts/ServiceRequest/IDisputedServiceRequest.sol
@@ -5,6 +5,5 @@ import "./Types.sol";
 
 interface IDisputedServiceRequest {
     function saveDisutedServiceRequest(address from, Types.ServiceRequestInfo memory serviceRequestInfo) external;
-    function getDisputedServiceRequestById(string memory _serviceRequestId) external view returns (Types.ServiceRequestInfo memory);
     function decideWinner(string memory _serviceRequestId) external returns (Types.ServiceRequestInfo memory);
 }
